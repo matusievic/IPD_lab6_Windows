432bef02d80c3c9b5b03034abf6f64c1bc108d2b
Andrei Matusevich <matusievic@gmail.com>
2017-12-07 03:00:48 +0300

    Scanner and Dispatcher classes were added

--- WirelessManager/WirelessManagerForm.Designer.cs
+++ WirelessManager/WirelessManagerForm.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.NetworksGridView = new System.Windows.Forms.DataGridView();
+            this.NameColumt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QualityColumt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AuthenticationColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MACColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.NetworksGridView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // NetworksGridView
+            // 
+            this.NetworksGridView.AllowUserToAddRows = false;
+            this.NetworksGridView.AllowUserToDeleteRows = false;
+            this.NetworksGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.NetworksGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumt,
+            this.QualityColumt,
+            this.AuthenticationColumn,
+            this.MACColumn});
+            this.NetworksGridView.Location = new System.Drawing.Point(0, -2);
+            this.NetworksGridView.Name = "NetworksGridView";
+            this.NetworksGridView.ReadOnly = true;
+            this.NetworksGridView.Size = new System.Drawing.Size(504, 240);
+            this.NetworksGridView.TabIndex = 0;
+            // 
+            // NameColumt
+            // 
+            this.NameColumt.HeaderText = "Name";
+            this.NameColumt.Name = "NameColumt";
+            this.NameColumt.ReadOnly = true;
+            // 
+            // QualityColumt
+            // 
+            this.QualityColumt.HeaderText = "Quality";
+            this.QualityColumt.Name = "QualityColumt";
+            this.QualityColumt.ReadOnly = true;
+            // 
+            // AuthenticationColumn
+            // 
+            this.AuthenticationColumn.HeaderText = "Authentication";
+            this.AuthenticationColumn.Name = "AuthenticationColumn";
+            this.AuthenticationColumn.ReadOnly = true;
+            // 
+            // MACColumn
+            // 
+            this.MACColumn.HeaderText = "MAC";
+            this.MACColumn.Name = "MACColumn";
+            this.MACColumn.ReadOnly = true;
             // 
             // WirelessManagerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(502, 311);
+            this.Controls.Add(this.NetworksGridView);
             this.Name = "WirelessManagerForm";
             this.Text = "Wireless Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.NetworksGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView NetworksGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QualityColumt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AuthenticationColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MACColumn;
     }
 }
 
--- WirelessManager/WirelessManagerForm.cs
+++ WirelessManager/WirelessManagerForm.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WirelessManager.Dispatcher;
 
 namespace WirelessManager
 {
     public partial class WirelessManagerForm : Form
     {
+        private WiFiDispatcher dispatcher = WiFiDispatcher.GetInstance();
         public WirelessManagerForm()
         {
             InitializeComponent();
+            NetworksGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            NetworksGridView.MultiSelect = false;
+            UpdateData();
+        }
+
+
+        private void UpdateData()
+        {
+            var networks = dispatcher.Networks;
+            NetworksGridView.Rows.Clear();
+            foreach(var network in networks)
+            {
+                NetworksGridView.Rows.Add(network.Name, network.ConnectionQuality, network.AuthenticationType, network.MAC);
+            }
         }
     }
 }
